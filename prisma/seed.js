7314efd0f7f30983adab834039ce2d555b242414
VentusAlpha <roxasbeta@gmail.com>
2021-04-10 19:22:52 -0600

    simplified seed file

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,53 +1,38 @@
-const {PrismaClient} = require("prisma");
+// const {PrismaClient} = require("prisma");
+import {PrismaClient} from "prisma"
 
-const animeData = require("../data/Anime.json");
-const mangaData = require("../data/Manga.json");
-const listData = require("../data/List.json");
+// const animeData = require("../data/Anime.json");
+// const mangaData = require("../data/Manga.json");
+// const listData = require("../data/List.json");
+
+import animeData from "../data/Anime.json"
+import mangaData from "../data/Manga.json"
+import Title from "../data/Title.json"
+import CoverImage from "../data/CoverImage.json"
+import listData from "../data/List.json"
 
 const prisma = new PrismaClient();
 
 async function main() {
   console.log("Start seeding...");
-  for (const i of animeData) {
+  for (const i of Title) {
     const title = await prisma.Title.create({
-      data: i.title,
-    });
-    const coverImage = await prisma.CoverImage.create({
-      data: i.coverImage,
-    });
-    let constructedAnime = {
-      idMal: i.idMal,
-      titleID: title.id,
-      description: i.description,
-      meanScore: i.meanScore,
-      genres: i.genres,
-      synonyms: i.synonyms,
-      source: i.source,
-      coverImageID: coverImage.id,
-    };
+      data: i
+    })
+  }
+  for (const i of CoverImage) {
+    const title = await prisma.CoverImage.create({
+      data: i
+    })
+  }
+  for (const i of animeData) {
     const anime = await prisma.Anime.create({
       data: constructedAnime
     });
   }
   for (const i of mangaData) {
-    const title = await prisma.Title.create({
-      data: i.title,
-    });
-    const coverImage = await prisma.CoverImage.create({
-      data: i.coverImage,
-    });
-    let constructedManga = {
-      idMal: i.idMal,
-      titleID: title.id,
-      description: i.description,
-      meanScore: i.meanScore,
-      genres: i.genres,
-      synonyms: i.synonyms,
-      source: i.source,
-      coverImageID: coverImage.id,
-    };
     const anime = await prisma.Manga.create({
-      data: constructedManga
+      data: i
     });
   }
   for (const i of listData) {
