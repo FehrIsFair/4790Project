df7c3f0b16b325966c7490f782ade97ce0a2f817
VentusAlpha <roxasbeta@gmail.com>
2021-04-24 18:37:48 -0600

    decided to go with the bcrypt and rest way of doing things

---
+++ controllers/api.controller.js
@@ -0,0 +1,15 @@
+import bcrypt from "bcrypt"
+
+import {Users} from "../models/user.model.js"
+
+const authUser = () => {
+
+};
+
+const createUser = () => {
+
+};
+
+const deleteUser = () => {
+
+};
