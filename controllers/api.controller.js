ce902b95ff86f0cfb8ad3f57749ede065e0b4fe0
VentusAlpha <roxasbeta@gmail.com>
2021-04-25 17:04:16 -0600

    Setup login api

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,15 +1,50 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
-import {Users} from "../models/user.model.js"
+import { Users } from "../models/user.model.js";
 
-const authUser = () => {
-
+export const authUser = async (req, res) => {
+  const user = await Users.find()
+    .where({ UserName: req.params.UserName })
+    .exec((err) => {
+      if (err) res.status(400).json({Message: `Could not find user ${err}`, auth: false });
+    });
+  if (user.Password === req.params.Password) {
+    res.status(200).json({
+      auth: true,
+    });
+  } else {
+    res.status(400).json({
+      auth: false,
+    });
+  }
 };
 
-const createUser = () => {
-
+export const createUser = async (req, res) => {
+  let newUser;
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(req.body.Password, salt, (err, hash) => {
+      if (err) res.status(400).json({Message: `Failed to create new user ${err}`});
+      newUser = new Users({
+        UserName: req.body.UserName,
+        Password: hash,
+      });
+    });
+  });
+  try {
+    newUser.save();
+    res.status(200).json({Message: "Created user", auth: true});
+  } catch (err) {
+    res.status(400).json({Message: `Could not create user ${err}`, auth: false});
+  }
 };
 
-const deleteUser = () => {
-
+export const deleteUser = async (req, res) => {
+  try {
+    Users.deleteOne().where({Username: req.body.UserName}).exec((err, user) => {
+      if (err) res.status(400).json({Message: `Could not delete user: ${err}`});
+      res.status(200).json({Message: "user Successfully deleted."});
+    });
+  } catch (err) {
+    res.status(400).json({Message: `Could not delete user: ${err}`})
+  }
 };
--- routes/api.route.js
+++ routes/api.route.js
@@ -2,29 +2,15 @@
 import {Router} from "express";
 
 import {
-  getAnime,
-  getAnimeByMalId,
-  getList,
-  getManga,
-  getMangaDetail,
-  editFavoriteList,
-  deleteFavoriteList,
-  createNewFavoriteList,
-  getAnimeDetail,
-  getMangaByMalId,
+  authUser,
+  createUser,
+  deleteUser
 } from "../controllers/api.controller.js";
 
 const apiRouter = Router();
 
-apiRouter.get("/Anime", getAnime);
-apiRouter.get("/Manga", getManga);
-apiRouter.get("/List/:uid", getList);
-apiRouter.get("/Anime/:mal_id", getAnimeByMalId);
-apiRouter.get("/Manga/:mal_id", getMangaByMalId);
-apiRouter.get("/Manga/Detail/:mal_id", getMangaDetail);
-apiRouter.get("/Anime/Detail/:mal_id", getAnimeDetail);
-apiRouter.put("/EditList", editFavoriteList);
-apiRouter.delete("/DeleteList/:_id", deleteFavoriteList);
-apiRouter.post("/CreateList", createNewFavoriteList);
+apiRouter.post("/Auth", authUser);
+apiRouter.post("/Create", createUser);
+apiRouter.delete("/Delete", deleteUser);
 
 export default apiRouter
